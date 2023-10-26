ee4d00effdf796c6b8926f038ca73ec968cfe107
MrRyhor <grigory.karpyak@gmail.com>
2023-10-26 15:49:23 +0200

    add gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-});
+    publicPath: `/lesson-6-task_5/`,
+    transpileDependencies: true,
+})
